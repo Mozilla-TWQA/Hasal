7d77f3308bb050436fd337656d1ad6a9d6cc74a7
Walter Chen <ypwalter@gmail.com>
2016-04-11 15:24:58 +0800

    adding comment for temp solution for environment setting up

--- tests/test_chrome_load_file.sikuli/test_chrome_load_file.py
+++ tests/test_chrome_load_file.sikuli/test_chrome_load_file.py
@@ -1,3 +1,7 @@
+# if you are putting your test script folders under {git project folder}/tests/, it will work fine.
+# otherwise, you either add it to system path before you run or hard coded it in here.
+# library_path = "/".join(getParentFolder().split("/")[:-2]) + "/lib/sikuli"
+# sys.path.append(library_path)
 import browser
 import common
 import sys
@@ -7,7 +11,7 @@ com = common.General()
 chrome = browser.Chrome()
 
 chrome.clickBar()
-chrome.enterLink("https://docs.google.com/document/d/1EpYUniwtLvBbZ4ECgT_vwGUfTHKnqSWi7vgNJQBemFk/edit?usp=sharing")
+chrome.enterLink("https://docs.google.com/document/d/1EpYUniwtLvBbZ4ECgT_vwGUfTHKnqSWi7vgNJQBemFk/edit")
 
 wait(6)
 chrome.getConsoleInfo("window.performance.timing")
