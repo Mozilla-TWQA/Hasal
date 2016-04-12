c6399d91d9d43310b444766032085c8c9b247353
shako <sho@mozilla.com>
2016-04-12 18:03:35 +0800

    add test case for gdoc load only

--- tests/test_firefox_gdoc_load.sikuli/test_firefox_gdoc_load.py
+++ tests/test_firefox_gdoc_load.sikuli/test_firefox_gdoc_load.py
@@ -1,9 +1,11 @@
 # if you are putting your test script folders under {git project folder}/tests/, it will work fine.
 # otherwise, you either add it to system path before you run or hard coded it in here.
-library_path = "/".join(getParentFolder().split("/")[:-2]) + "/lib/sikuli"
-sys.path.append(library_path)
 import browser
+import common
 
+
+com = common.General()
 ff = browser.Firefox()
+
 ff.clickBar()
-ff.enterLink("http://goo.gl/3GR0GN")
+ff.enterLink("https://goo.gl/SEpp2A?hl=en")
