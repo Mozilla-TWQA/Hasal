2b80af736bf1205e109eb2c3e8193c3bd7afc251
mikel <mikel@mikel-BM6875-BM6675-BP6375.(none)>
2016-04-19 11:17:17 +0800

    add create gdoc copy paste image case

---
+++ tests/test_chrome_gdoc_create_copypaste_image_1.sikuli/test_chrome_gdoc_create_copypaste_image_1.py
@@ -0,0 +1,33 @@
+# if you are putting your test script folders under {git project folder}/tests/, it will work fine.
+# otherwise, you either add it to system path before you run or hard coded it in here.
+sys.path.append(sys.argv[2])
+import browser
+import common
+import gdoc
+
+com = common.General()
+chrome = browser.Chrome()
+gd = gdoc.gDoc()
+
+chrome.clickBar()
+chrome.enterLink(sys.argv[3])
+sleep(5)
+gd.wait_for_loaded()
+gd.insert_image_url("https://drive.google.com/file/d/0B9Zi9TqbRWsdTV9JTmZQUXRFTWM/view?usp=sharing")
+sleep(5)
+com.select_all()
+sleep(1)
+com.cut()
+sleep(1)
+com.paste()
+sleep(1)
+type(Key.ENTER)
+type(Key.ENTER)
+com.select_all() 
+sleep(1)
+com.copy()
+sleep(1)
+type(Key.RIGHT)
+sleep(1)
+com.paste()
+sleep(1)
