37f19ffd21a217645126988d798ad4d94a18b60d
mikel <mikel@mikel-BM6875-BM6675-BP6375.(none)>
2016-04-19 09:39:51 +0800

    fix focus method mismatch problem

--- tests/test_chrome_gdoc_create_image_1.py
+++ tests/test_chrome_gdoc_create_image_1.py
@@ -10,7 +10,7 @@ class TestSikuli(PerfBaseTest):
         self.test_url, self.test_url_id = self.target_helper.clone_target("1zSZRnZA8LsXcj29nuyd7xya9M5VcClKdFEOOx2cSVyU",
                                                                           self.env.output_name)
         time.sleep(3)
-        self.sikuli.run(self.env.sikuli_path, self.env.hasal_dir, "test_firefox_switchcontentwindow",
+        self.sikuli.run(self.env.sikuli_path, self.env.hasal_dir, "test_chrome_switchcontentwindow",
                         self.env.test_method_name + "_" + self.env.time_stamp)
 
         time.sleep(3)
@@ -24,7 +24,7 @@ class TestSikuli(PerfBaseTest):
 
     def tearDown(self):
         time.sleep(3)
-        self.sikuli.run(self.env.sikuli_path, self.env.hasal_dir, "test_firefox_defocuscontentwindow",
+        self.sikuli.run(self.env.sikuli_path, self.env.hasal_dir, "test_chrome_defocuscontentwindow",
                         self.env.test_method_name + "_" + self.env.time_stamp)
         time.sleep(3)
         captureHelper.capture_screen(self.env, self.env.video_output_sample_2_fp, self.env.img_sample_dp,
