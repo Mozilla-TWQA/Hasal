911caf9924802ee8507fb2d28be0e035c99ee98f
Cynthia Tang <ctang@mozilla.com>
2016-04-21 14:27:16 +0800

    Change URL

--- tests/test_firefox_gdoc_delete_basic_image_1.py
+++ tests/test_firefox_gdoc_delete_basic_image_1.py
@@ -7,7 +7,7 @@ class TestSikuli(PerfBaseTest):
 
     def setUp(self):
         super(TestSikuli, self).setUp()
-        self.test_url, self.test_url_id = self.target_helper.clone_target("1o7zKj0oUYj0hyC_jKxVJOcCuy4xcf0cUiA4Jac-zqZk",
+        self.test_url, self.test_url_id = self.target_helper.clone_target("1w_WrGwQ-45x-kxa_lga-ptGLUBvRFtLsfX14ZB9vDhI",
                                                                           self.env.output_name)
         time.sleep(3)
         self.sikuli.run(self.env.sikuli_path, self.env.hasal_dir, "test_firefox_switchcontentwindow",
@@ -17,7 +17,7 @@ class TestSikuli(PerfBaseTest):
         captureHelper.capture_screen(self.env, self.env.video_output_sample_1_fp, self.env.img_sample_dp,
                                      self.env.img_output_sample_1_fn)
 
-    def test_firefox_gdoc_delete_basic_txt_1(self):
+    def test_firefox_gdoc_delete_basic_image_1(self):
         self.sikuli.run(self.env.sikuli_path, self.env.hasal_dir, self.env.test_method_name,
                         self.env.test_method_name + "_" + self.env.time_stamp, test_url=self.test_url)
         assert(True)
