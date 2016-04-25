7c0058969cec4ca22347e7106b9909790c5e9e05
mikel <mikel@mikel-BM6875-BM6675-BP6375.(none)>
2016-04-25 16:34:23 +0800

    check sikuli status in shell script

--- runtest.sh
+++ runtest.sh
@@ -12,14 +12,14 @@ while IFS='' read -r case_name || [[ -n "$case_name" ]]; do
   fi
   COUNTER=0
   RETRY_COUNTER=0
-  while [ $COUNTER -lt 30 ]; do
+  while [ $COUNTER -lt 40 ]; do
     pkill firefox
     pkill chrome
     pkill avconv
     echo "The counter is $COUNTER and the retry_counter is $RETRY_COUNTER"
     python -m unittest tests.$case_name_no_space
-    out=$?
-    if [ "$out" -eq 0 ]; then
+    sikuli_stat=$(head -n 1 sikuli_stat.txt)
+    if [ "$sikuli_stat" -eq 0 ]; then
       let COUNTER=COUNTER+1
     else
       let RETRY_COUNTER=RETRY_COUNTER+1
