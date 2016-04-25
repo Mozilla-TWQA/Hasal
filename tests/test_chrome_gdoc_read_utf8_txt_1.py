995467d1e6d81953b62c3266bb568cb3fc6b951c
shako <sho@mozilla.com>
2016-04-25 17:48:05 +0800

    remove assert during execution

--- tests/test_chrome_gdoc_read_utf8_txt_1.py
+++ tests/test_chrome_gdoc_read_utf8_txt_1.py
@@ -12,4 +12,3 @@ class TestSikuli(PerfBaseTest):
         self.sikuli_status = self.sikuli.run(self.env.sikuli_path, self.env.hasal_dir, self.env.test_method_name,
                                              self.env.test_method_name + "_" + self.env.time_stamp,
                                              test_url=self.test_url)
-        assert(self.sikuli_status == 0)
--- tests/test_firefox_gdoc_create_copypaste_image_1.py
+++ tests/test_firefox_gdoc_create_copypaste_image_1.py
@@ -11,4 +11,3 @@ class TestSikuli(PerfBaseTest):
     def test_firefox_gdoc_create_copypaste_image_1(self):
         self.sikuli_status = self.sikuli.run(self.env.sikuli_path, self.env.hasal_dir, self.env.test_method_name,
                         self.env.test_method_name + "_" + self.env.time_stamp, test_url=self.test_url)
-        assert(self.sikuli_status == 0)
