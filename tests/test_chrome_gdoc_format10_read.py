203583ecbade05dc243e66810204ae3fb6965d05
Walter Chen <wachen@mozilla.com>
2016-04-29 18:22:53 +0800

    modified errors

--- tests/test_chrome_gdoc_format10_read.py
+++ tests/test_chrome_gdoc_format10_read.py
@@ -8,6 +8,6 @@ class TestSikuli(PerfBaseTest):
         self.test_url, self.test_url_id = self.target_helper.clone_target(self.env.TEST_TARGET_ID_10_FORMAT,
                                                                           self.env.output_name)
 
-    def test_chrome_gdoc_findr3_read(self):
+    def test_chrome_gdoc_format10_read(self):
         self.sikuli_status = self.sikuli.run(self.env.sikuli_path, self.env.hasal_dir, self.env.test_method_name,
                         self.env.test_method_name + "_" + self.env.time_stamp, test_url=self.test_url)
