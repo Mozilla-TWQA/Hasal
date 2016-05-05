58d6437d20d8bdaef7973a32f92e1f1846e8122a
shako <sho@mozilla.com>
2016-05-05 14:53:22 +0800

    add time to sleep when har file is not generated on time

--- lib/profiler/harProfiler.py
+++ lib/profiler/harProfiler.py
@@ -1,5 +1,5 @@
 import os
-from ..helper.desktopHelper import DEFAULT_BROWSER_TYPE_FIREFOX
+import time
 from base import BaseProfiler
 
 
@@ -10,14 +10,18 @@ class HarProfiler(BaseProfiler):
 
     def stop_recording(self, **kwargs):
         har_dir_path = os.path.join(kwargs['profile_path'], "har", "logs")
-        if os.path.exists(har_dir_path):
-            har_file_list = os.listdir(har_dir_path)
-            if len(har_file_list) == 1:
-                har_file_path = os.path.join(har_dir_path, har_file_list[0])
-                os.rename(har_file_path, self.env.profile_har_file_fp)
-            elif len(har_file_list) == 0:
-                print "[ERROR] can't find any har file in log folder %s" % har_dir_path
+        for i in range(10):
+            if os.path.exists(har_dir_path):
+                har_file_list = os.listdir(har_dir_path)
+                if len(har_file_list) == 1:
+                    har_file_path = os.path.join(har_dir_path, har_file_list[0])
+                    os.rename(har_file_path, self.env.profile_har_file_fp)
+                    break
+                elif len(har_file_list) == 0:
+                    print "[ERROR] can't find any har file in log folder %s" % har_dir_path
+                else:
+                    print "[ERROR] find more than one har file in log folder %s" % har_dir_path
             else:
-                print "[ERROR] find more than one har file in log folder %s" % har_dir_path
-        else:
-            print "[ERROR] har log folder is not exist %s " % har_dir_path
+                print "[ERROR] har log folder is not exist %s " % har_dir_path
+
+            time.sleep(1)
