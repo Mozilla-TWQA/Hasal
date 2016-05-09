d8ba8194d7c8d6fd7cf75120a72a0308ff1e8fed
Cynthia Tang <cynthia@Cynthias-MacBook-Pro.local>
2016-05-09 16:10:59 +0800

    Add test cases for Read & Basic

---
+++ tests/test_firefox_gdoc_read_basic_3_increasing.py
@@ -0,0 +1,14 @@
+from lib.perfBaseTest import PerfBaseTest
+
+
+class TestSikuli(PerfBaseTest):
+
+    def setUp(self):
+        super(TestSikuli, self).setUp()
+        self.test_url, self.test_url_id = self.target_helper.clone_target(self.env.TEST_TARGET_ID_3_PAGE_CONTENT_WITH_TXT_TABLE_IMAGE_INCREASING,
+                                                                          self.env.output_name)
+
+    def test_firefox_gdoc_read_basic_3_increasing(self):
+        self.sikuli_status = self.sikuli.run(self.env.sikuli_path, self.env.hasal_dir, self.env.test_method_name,
+                                             self.env.test_method_name + "_" + self.env.time_stamp,
+                                             test_url=self.test_url)
