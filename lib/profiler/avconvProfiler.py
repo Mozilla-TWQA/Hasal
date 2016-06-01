aae720d95ca7b79cec4fb63a9715507d5bfdcd8d
Walter Chen <ypwalter@gmail.com>
2016-06-01 16:30:18 +0800

    added

--- lib/profiler/avconvProfiler.py
+++ lib/profiler/avconvProfiler.py
@@ -16,12 +16,12 @@ class AvconvProfiler(BaseProfiler):
         if platform.system().lower() == "windows":
             self.process = subprocess.Popen("ffmpeg -f gdigrab -draw_mouse 0 -framerate 1000 -video_size 1024*768 -i desktop -c:v libx264 -preset ultrafast -r 100 " + self.env.video_output_fp)
         else:
-		        self.process = subprocess.Popen(
-		            video_capture_line(self.env.DEFAULT_VIDEO_RECORDING_FPS, self.env.DEFAULT_VIDEO_RECORDING_POS_X,
-		                               self.env.DEFAULT_VIDEO_RECORDING_POS_Y,
-		                               self.env.DEFAULT_VIDEO_RECORDING_WIDTH, self.env.DEFAULT_VIDEO_RECORDING_HEIGHT,
-		                               self.env.DEFAULT_VIDEO_RECORDING_DISPLAY,
-		                               self.env.DEFAULT_VIDEO_RECORDING_CODEC, self.env.video_output_fp))
+            self.process = subprocess.Popen(
+                video_capture_line(self.env.DEFAULT_VIDEO_RECORDING_FPS, self.env.DEFAULT_VIDEO_RECORDING_POS_X,
+                self.env.DEFAULT_VIDEO_RECORDING_POS_Y,
+                self.env.DEFAULT_VIDEO_RECORDING_WIDTH, self.env.DEFAULT_VIDEO_RECORDING_HEIGHT,
+                self.env.DEFAULT_VIDEO_RECORDING_DISPLAY,
+                self.env.DEFAULT_VIDEO_RECORDING_CODEC, self.env.video_output_fp))
 
     def stop_recording(self, **kwargs):
         if platform.system().lower() == "windows":
