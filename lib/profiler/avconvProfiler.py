5d04a1583da31d89ab5ded365b5f66af4a6dbd86
Walter Chen <ypwalter@gmail.com>
2016-06-03 14:34:41 +0800

    finalized python ffmpeg command

--- lib/profiler/avconvProfiler.py
+++ lib/profiler/avconvProfiler.py
@@ -14,7 +14,7 @@ class AvconvProfiler(BaseProfiler):
             os.remove(self.env.video_output_fp)
 
         if platform.system().lower() == "windows":
-            self.process = subprocess.Popen("ffmpeg -f gdigrab -draw_mouse 0 -framerate 1000 -video_size 1024*768 -i desktop -c:v libx264 -preset ultrafast -r 100 " + self.env.video_output_fp)
+            self.process = subprocess.Popen("ffmpeg -f gdigrab -draw_mouse 0 -framerate 90 -video_size 1024*768 -i desktop -c:v libx264 -preset veryfast -g 15 -crf 0 " + self.env.video_output_fp)
         else:
             self.process = subprocess.Popen(
                 video_capture_line(self.env.DEFAULT_VIDEO_RECORDING_FPS, self.env.DEFAULT_VIDEO_RECORDING_POS_X,
