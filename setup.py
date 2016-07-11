c36289fceded7211e54c37f4c22d132b185378e6
shako <sho@mozilla.com>
2016-07-11 13:07:49 +0800

    update-setup.py-support-platform-dep

--- setup.py
+++ setup.py
@@ -3,11 +3,27 @@
 
 # from distutils.sysconfig import get_python_lib
 import os
+import platform
 from setuptools import setup, find_packages
 
+DEFAULT_REQUIREMENT_DOC = "requirements.txt"
+DEFAULT_REQUIREMENT_DOC_FOR_WIN = "requirements_windows.txt"
+DEFAULT_REQUIREMENT_DOC_FOR_MAC = "requirements_mac.txt"
+
+
+
 # dependencies
-with open('requirements.txt') as f:
+with open(DEFAULT_REQUIREMENT_DOC) as f:
     deps = f.read().splitlines()
+
+if platform.system().lower() == "windows":
+    if os.path.exists(DEFAULT_REQUIREMENT_DOC_FOR_WIN):
+        with open(DEFAULT_REQUIREMENT_DOC_FOR_WIN) as fh_win:
+            deps.extend(fh_win.read().splitlines())
+elif platform.system().lower() == "darwin":
+    if os.path.exists(DEFAULT_REQUIREMENT_DOC_FOR_MAC):
+        with open(DEFAULT_REQUIREMENT_DOC_FOR_MAC) as fh_mac:
+            deps.extend(fh_mac.read().splitlines())
 
 version = "0.1.0"
 
