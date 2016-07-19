754eca437d09b36f7fba9b75b4f44decdb1b167c
Walter Chen <wachen@mozilla.com>
2016-07-19 17:10:20 +0800

    modify image for compatible with windows

--- tests/facebook_pilot/test_firefox_facebook_enlarge_video_1.sikuli/test_firefox_facebook_enlarge_video_1.py
+++ tests/facebook_pilot/test_firefox_facebook_enlarge_video_1.sikuli/test_firefox_facebook_enlarge_video_1.py
@@ -5,5 +5,4 @@ type(Key.ENTER)
 setAutoWaitTimeout(10)
 wait("1468830683686.png")
 doubleClick(Pattern("1468577119085.png").targetOffset(0,200))
-wait("1468830169738.png",240)
-
+wait("1468914267501.png",240)
