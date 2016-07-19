28102c3767495951e55eadafcb09cab369ad5280
askeing <askeing@gmail.com>
2016-07-19 17:58:09 +0800

    Add Gmail Pilot - View PDF

---
+++ tests/GMail/test_chrome_view_pdf.sikuli/test_chrome_view_pdf.py
@@ -0,0 +1,14 @@
+# View the PDF attachment
+
+wait("1466662802234-2.png")
+paste("1466662802234-2.png", "https://mail.google.com/mail/u/0/#label/GMail_Pilot/1560292364b6f94c")
+type(Key.ENTER)
+wait(5)
+
+# Select the attachment
+wait("1468922053587.png")
+hover("1468922053587.png")
+click("1468922053587.png")
+
+# display PDF
+wait("1468922131651.png", 10)
