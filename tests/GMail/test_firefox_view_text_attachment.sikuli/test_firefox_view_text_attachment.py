95639857ae323ad081d183aa99c93d628d024c0f
askeing <askeing@gmail.com>
2016-07-19 16:28:08 +0800

    Add Gmail Pilot - View Text Attachment

---
+++ tests/GMail/test_firefox_view_text_attachment.sikuli/test_firefox_view_text_attachment.py
@@ -0,0 +1,13 @@
+# View the text file that your friend shared with you
+
+wait("1468896948832.png")
+paste("1468896948832-1.png", "https://mail.google.com/mail/u/0/#label/GMail_Pilot/15602320b54cde4f")
+type(Key.ENTER)
+wait(5)
+
+# Select a mail that you want to read
+wait("1468915870038.png")
+hover("1468915870038.png")
+click("1468915870038.png")
+
+wait(Pattern("1468916016557.png").similar(0.60))
