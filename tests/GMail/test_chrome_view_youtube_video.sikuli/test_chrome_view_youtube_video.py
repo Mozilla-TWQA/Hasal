d6928583230d1776d293eb5fc97bebab12623949
askeing <askeing@gmail.com>
2016-07-19 17:35:24 +0800

    Add Gmail Pilot - Youtube Video

---
+++ tests/GMail/test_chrome_view_youtube_video.sikuli/test_chrome_view_youtube_video.py
@@ -0,0 +1,14 @@
+# View the Video attachment
+
+wait("1466662802234-2.png")
+paste("1466662802234-2.png", "https://mail.google.com/mail/u/0/#label/GMail_Pilot/1560272d8852c488")
+type(Key.ENTER)
+wait(5)
+
+# Select the attachment
+wait("1468920153547.png")
+hover("1468920153547.png")
+click("1468920182335.png")
+
+# play video
+wait("1468920241331.png", 2*60)
---
+++ tests/GMail/test_firefox_view_youtube_video.sikuli/test_firefox_view_youtube_video.py
@@ -0,0 +1,14 @@
+# View the Video attachmen
+
+wait("1468896948832.png")
+paste("1468896948832-1.png", "https://mail.google.com/mail/u/0/#label/GMail_Pilot/1560272d8852c488")
+type(Key.ENTER)
+wait(5)
+
+# Select the attachment
+wait("1468920153547.png")
+hover("1468920153547.png")
+click("1468920182335.png")
+
+# play video
+wait("1468920241331.png", 2*60)
