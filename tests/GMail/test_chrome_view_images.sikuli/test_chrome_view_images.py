d50017e7fe15f4b1a940ccaff795b5b846b918d1
askeing <askeing@gmail.com>
2016-07-22 11:40:05 +0800

    Add CC-BY-SA 4.0 license for View Images tests

--- tests/GMail/test_chrome_view_images.sikuli/test_chrome_view_images.py
+++ tests/GMail/test_chrome_view_images.sikuli/test_chrome_view_images.py
@@ -1,8 +1,11 @@
 # View the Images
 # Images:
 # - https://commons.wikimedia.org/wiki/File:Lobo_marino_(Zalophus_californianus_wollebaeki),_Punta_Pitt,_isla_de_San_Crist%C3%B3bal,_islas_Gal%C3%A1pagos,_Ecuador,_2015-07-24,_DD_11.JPG
+#   - Diego Delso, Wikimedia Commons, License CC-BY-SA 4.0
 # - https://commons.wikimedia.org/wiki/File:Lobo_marino_(Zalophus_californianus_wollebaeki),_Punta_Pitt,_isla_de_San_Crist%C3%B3bal,_islas_Gal%C3%A1pagos,_Ecuador,_2015-07-24,_DD_12.JPG
+#   - Diego Delso, Wikimedia Commons, License CC-BY-SA 4.0
 # - https://commons.wikimedia.org/wiki/File:Narcissus_poeticus_Spechtensee_01.JPG
+#   - Uoaei1, Wikimedia Commons, License CC-BY-SA 4.0
 
 wait("1469080970628.png")
 paste("1469080970628.png", "https://mail.google.com/mail/u/0/#label/GMail_Pilot")
