93e943fceacae63f02f4fb90067dfef502fee517
Shako <sho@mozilla.com>
2016-07-26 16:25:55 +0800

    add no http2 flag

--- lib/profiler/mitmDumpProfiler.py
+++ lib/profiler/mitmDumpProfiler.py
@@ -9,7 +9,7 @@ class MitmDumpProfiler(BaseProfiler):
     process = None
 
     def start_recording(self):
-        default_dump_flow_cmd = ["mitmdump", "-w"]
+        default_dump_flow_cmd = ["mitmdump", "--no-http2", "-w"]
         flow_file_fp = os.path.join(self.env.DEFAULT_FLOW_DIR, self.env.flow_file_fp)
         if not os.path.exists(self.env.DEFAULT_FLOW_DIR):
             os.mkdir(self.env.DEFAULT_FLOW_DIR)
@@ -27,7 +27,7 @@ class MitmDumpProfiler(BaseProfiler):
                 else:
                     print "According to your answer, we will not record the packet during this test!"
         else:
-            replay_cmd_list = ["mitmdump", "-S", flow_file_fp, "--no-pop", "--norefresh"]
+            replay_cmd_list = ["mitmdump", "--no-http2", "-S", flow_file_fp, "--no-pop", "--norefresh"]
             self.process = subprocess.Popen(replay_cmd_list)
 
     def stop_recording(self, **kwargs):
