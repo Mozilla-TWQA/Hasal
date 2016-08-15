59391c4f0b5543de1bdfbe650a70ec7adfe0eb0e
askeing <askeing@gmail.com>
2016-08-15 14:30:27 +0800

    Rename the screenshots

--- lib/sikuli/facebook.sikuli/facebook.py
+++ lib/sikuli/facebook.sikuli/facebook.py
@@ -16,33 +16,33 @@ class facebook():
     def wait_for_loaded(self):
         default_timeout = getAutoWaitTimeout()
         setAutoWaitTimeout(10)
-        wait(Pattern("1470820258980.png").similar(0.85), 10)
+        wait(Pattern("pics/facebook_logo.png").similar(0.85), 10)
         setAutoWaitTimeout(default_timeout)
         self.focus_window()
 
     def focus_window(self):
         default_timeout = getAutoWaitTimeout()
         setAutoWaitTimeout(10)
-        click(Pattern("1470820258980.png").similar(0.85).targetOffset(0,15))
+        click(Pattern("pics/facebook_logo.png").similar(0.85).targetOffset(0,15))
         setAutoWaitTimeout(default_timeout)
 
     def post_url(self):
-        click(Pattern("1470818721451.png").similar(0.85))
+        click(Pattern("pics/facebook_home_post_area.png").similar(0.85))
         paste('https://en.wikipedia.org/wiki/Sun_Tzu ')
-        wait("1470822122151.png", 10)
-        click(Pattern("1470818889053.png").similar(0.85))
-        wait(Pattern("1470821414750.png").similar(0.85), 10)
+        wait("pics/facebook_post_url_thumbnail.png", 10)
+        click(Pattern("pics/facebook_post_button.png").similar(0.85))
+        wait(Pattern("pics/facebook_post_url_result.png").similar(0.85), 10)
         print('[Facebook] post_url() done.')
 
     def post_url_del(self):
-        wait(Pattern("1470821414750.png").similar(0.85), 10)
-        click(Pattern("1470821414750.png").similar(0.85).targetOffset(230,-200))
-        wait(Pattern("1470822426794.png").similar(0.85), 10)
-        click(Pattern("1470822426794.png").similar(0.85).targetOffset(-70,-70))
-        wait(Pattern("1470821690209.png").similar(0.85), 10)
-        click(Pattern("1470821690209.png").similar(0.85).targetOffset(160,50))
-        waitVanish(Pattern("1470821690209.png").similar(0.85), 10)
-        waitVanish(Pattern("1470821414750.png").similar(0.85), 10)
+        wait(Pattern("pics/facebook_post_url_result.png").similar(0.85), 10)
+        click(Pattern("pics/facebook_post_url_result.png").similar(0.85).targetOffset(230,-200))
+        wait(Pattern("pics/facebook_delete_post_menu.png").similar(0.85), 10)
+        click(Pattern("pics/facebook_delete_post_menu.png").similar(0.85).targetOffset(-70,-70))
+        wait(Pattern("pics/facebook_delete_post_comfirm_dialog.png").similar(0.85), 10)
+        click(Pattern("pics/facebook_delete_post_comfirm_dialog.png").similar(0.85).targetOffset(160,50))
+        waitVanish(Pattern("pics/facebook_delete_post_comfirm_dialog.png").similar(0.85), 10)
+        waitVanish(Pattern("pics/facebook_post_url_result.png").similar(0.85), 10)
         print('[Facebook] post_url_del() done.')
 
 
