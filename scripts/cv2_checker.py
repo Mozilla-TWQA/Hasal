9da85e088613e1eced5a784082f1c59bd9791997
Askeing Yen (fyen) <askeing@gmail.com>
2016-09-06 11:36:52 +0800

    Update cv2_checker.py

--- scripts/cv2_checker.py
+++ scripts/cv2_checker.py
@@ -13,7 +13,7 @@ class CV2Checker(object):
         # This video file comes from B2G Gaia repo
         # - https://github.com/mozilla-b2g/gaia/blob/master/test_media/Movies/gizmo2.mp4
         self.input_video = os.path.join(self.current_file_dir, 'media', '168_frame.mp4')
-        self.input_video_frame = 168
+        self.input_video_frame_base = 1
 
     def check_convert_video_to_images(self):
         video_capture = cv2.VideoCapture(self.input_video)
@@ -23,7 +23,7 @@ class CV2Checker(object):
         while ret:
             ret, _ = video_capture.read()
             counter += 1
-        assert self.input_video_frame == counter, 'The video [{}] frame should be 168.'.format(self.input_video)
+        assert counter > self.input_video_frame_base , 'The video [{}] frame should be more than {}, not {}.'.format(self.input_video, self.input_video_frame_base, counter)
 
 
 def main():
