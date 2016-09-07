92091ff55ade0349100dfd8c71e75645051a2d4b
askeing <askeing@gmail.com>
2016-09-07 16:30:51 +0800

    Add sikulix into bootstrap

--- bootstrap-linux.sh
+++ bootstrap-linux.sh
@@ -28,22 +28,35 @@ else
 fi
 
 
-func_log "[INFO] Install Requiremants ..."
+func_log "[INFO] Install Requirements ..."
+
 # tools
+func_log "[INFO] Installing tools ..."
 sudo apt-get install -y --force-yes unzip wget git
 # python
+func_log "[INFO] Installing python ..."
 sudo apt-get install -y --force-yes python-dev python-virtualenv python-pip
-# build toolchains
+# build toolchain
+func_log "[INFO] Installing build toolchain ..."
 sudo apt-get install -y build-essential cmake libffi-dev libssl-dev
 # req of numpy, scipy
+func_log "[INFO] Installing req of numpy, scipy ..."
 sudo apt-get install -y --force-yes libblas-dev liblapack-dev libatlas-base-dev gfortran
-# opencv
+# OpenCV
+func_log "[INFO] Installing OpenCV ..."
 sudo apt-get install -y --force-yes libopencv-dev python-opencv
-func_log "[INFO] Install Requiremants finished."
 # imagemagick, for Speed Index
+func_log "[INFO] Installing imagemagick ..."
 sudo apt-get install -y --force-yes imagemagick
 # mitmproxy
+func_log "[INFO] Installing mitmproxy ..."
 sudo apt-get install -y --force-yes mitmproxy
+# sikulix
+func_log "[INFO] Installing Sikulix 1.1.0 ..."
+wget -P thirdParty/ https://launchpad.net/sikuli/sikulix/1.1.0/+download/sikulixsetup-1.1.0.jar
+java -jar thirdParty/sikulixsetup-1.1.0.jar options 1.1
+
+func_log "[INFO] Install Requirements finished."
 
 ################
 # Installation #
--- bootstrap-mac.sh
+++ bootstrap-mac.sh
@@ -59,18 +59,11 @@ do
 done
 func_log "[PASS] Checking finished."
 
-
 ################
 # Installation #
 ################
 
-func_log "[INFO] Creating virtualenv ..."
-virtualenv .env-python
-source .env-python/bin/activate
-
-func_log "[INFO] Upgrading pip itself ..."
-pip install -U pip
-
+func_log "[INFO] Installing Requirements ..."
 func_log "[INFO] Running brew install homebrew/science ..."
 # updating brew before tap homebrew/science
 brew update
@@ -79,32 +72,49 @@ brew update
 
 # ffmpeg, skip on CI
 if [[ ${TRAVIS} ]]; then
-    func_log "[WARN] Skip brew install ffmpeg on Travis CI, due to it is very slow!"
+    func_log "[WARN] Skip installing ffmpeg on Travis CI, due to it is very slow!"
 else
-    func_log "[INFO] Running brew install ffmpeg ..."
+    func_log "[INFO] Installing ffmpeg ..."
     brew install ffmpeg --with-fdk-aac --with-ffplay --with-freetype --with-frei0r --with-libass --with-libvo-aacenc --with-libvorbis --with-libvpx --with-opencore-amr --with-openjpeg --with-opus --with-rtmpdump --with-schroedinger --with-speex --with-theora --with-tools
 fi
 
 # libav (avconv)
-func_log "[INFO] Running brew install libav ..."
+func_log "[INFO] Installing libav ..."
 brew install libav
 
 # OpenCV
-func_log "[INFO] Running brew install opencv ..."
+func_log "[INFO] Installing opencv ..."
 brew install homebrew/science/opencv
-func_log "[INFO] Linking opencv's cv2.so to virtualenv ..."
-CV2_SO_PATH=`find /usr/local/Cellar/opencv/ -name "cv2.so"`
-ln -s ${CV2_SO_PATH} .env-python/lib/python2.7/site-packages/cv2.so
 
 # imagemagick, for Speed Index
+func_log "[INFO] Installing imagemagick ..."
 brew install imagemagick
 
 # mitmproxy
+func_log "[INFO] Installing mitmproxy ..."
 brew install mitmproxy
+
+# sikulix
+func_log "[INFO] Installing Sikulix 1.1.0 ..."
+wget -P thirdParty/ https://launchpad.net/sikuli/sikulix/1.1.0/+download/sikulixsetup-1.1.0.jar
+java -jar thirdParty/sikulixsetup-1.1.0.jar options 1.1
+
+func_log "[INFO] Install Requirements finished."
 
 ###############
 # Hasal Setup #
 ###############
+
+func_log "[INFO] Creating virtualenv ..."
+virtualenv .env-python
+source .env-python/bin/activate
+
+func_log "[INFO] Upgrading pip itself ..."
+pip install -U pip
+
+func_log "[INFO] Linking opencv's cv2.so to virtualenv ..."
+CV2_SO_PATH=`find /usr/local/Cellar/opencv/ -name "cv2.so"`
+ln -s ${CV2_SO_PATH} .env-python/lib/python2.7/site-packages/cv2.so
 
 func_log "[INFO] Python Setup Install ..."
 pip install -r requirements.txt
