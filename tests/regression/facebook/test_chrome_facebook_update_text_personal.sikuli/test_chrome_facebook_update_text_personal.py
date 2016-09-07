bb3efeaad3786a4d3cc8fefcdab4b293ea839c72
Mike Lien <mlien@mozilla.com>
2016-09-07 14:41:34 +0800

    add update and delete post cases

---
+++ tests/regression/facebook/test_chrome_facebook_update_text_personal.sikuli/test_chrome_facebook_update_text_personal.py
@@ -0,0 +1,25 @@
+# if you are putting your test script folders under {git project folder}/tests/, it will work fine.
+# otherwise, you either add it to system path before you run or hard coded it in here.
+sys.path.append(sys.argv[2])
+import browser
+import common
+import facebook
+
+com = common.General()
+chrome = browser.Firefox()
+fb = facebook.facebook()
+
+chrome.clickBar()
+chrome.enterLink(sys.argv[3])
+
+sleep(2)
+setAutoWaitTimeout(10)
+fb.wait_for_loaded()
+click(fb.non_club_post_marker.targetOffset(75, 35))
+click(fb.non_club_post_menu_edit)
+wait(fb.save_button)
+type(Key.ENTER)
+paste('add new line')
+sleep(1)
+click(fb.save_button)
+wait(fb.personal_post_area)
---
+++ tests/regression/facebook/test_firefox_facebook_delete_post_club.sikuli/test_firefox_facebook_delete_post_club.py
@@ -0,0 +1,22 @@
+# if you are putting your test script folders under {git project folder}/tests/, it will work fine.
+# otherwise, you either add it to system path before you run or hard coded it in here.
+sys.path.append(sys.argv[2])
+import browser
+import common
+import facebook
+
+com = common.General()
+ff = browser.Firefox()
+fb = facebook.facebook()
+
+ff.clickBar()
+ff.enterLink(sys.argv[3])
+
+sleep(2)
+setAutoWaitTimeout(10)
+fb.wait_for_loaded()
+click(fb.club_post_marker.targetOffset(55, 75))
+click(fb.club_post_menu_delete)
+sleep(1)
+click(fb.club_delete_post_button)
+wait(fb.club_post_area)
