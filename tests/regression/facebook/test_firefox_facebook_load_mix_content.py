9c0811edcf24420728097cbed17c00ffb69200f1
Mike Lien <mlien@mozilla.com>
2016-09-08 15:50:51 +0800

    fix fb load mix content

--- tests/regression/facebook/test_firefox_facebook_load_mix_content.py
+++ tests/regression/facebook/test_firefox_facebook_load_mix_content.py
@@ -7,4 +7,4 @@ class TestSikuli(PerfBaseTest):
         super(TestSikuli, self).setUp()
 
     def test_firefox_facebook_load_mix_content(self):
-        self.sikuli_status = self.sikuli.run_test(self.env.test_name, self.env.output_name, test_target=self.env.TEST_FB_MIX_CONTENT, script_dp=self.env.test_script_py_dp)
+        self.sikuli_status = self.sikuli.run_test(self.env.test_name, self.env.output_name, test_target=self.env.TEST_FB_MIX_CONTENT_POST, script_dp=self.env.test_script_py_dp)
