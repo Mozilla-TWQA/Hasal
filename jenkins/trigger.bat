04cfd9d4ab0afd7f8229bc0c9b1b5c6fae173452
mikelien <mlien@mozilla.com>
2016-10-21 12:07:12 +0800

    add jenkins scripts

---
+++ jenkins/trigger.bat
@@ -0,0 +1,10 @@
+::HASAL_WORKSPACE is the Hasal path in current environment
+
+cd %HASAL_WORKSPACE%
+
+git checkout master
+pip install treeherder-client==3.0.0
+pip install docopt
+
+git pull
+python tools\trigger_build.py
