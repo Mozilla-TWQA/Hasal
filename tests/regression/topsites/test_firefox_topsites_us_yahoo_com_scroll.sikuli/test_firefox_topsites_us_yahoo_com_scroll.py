a8b4caaf7d1b0acda18729873df85f62c2f54196
Askeing <askeing@gmail.com>
2016-11-04 13:13:31 +0800

    Add Top Sites cases

---
+++ tests/regression/topsites/test_firefox_topsites_us_yahoo_com_scroll.sikuli/test_firefox_topsites_us_yahoo_com_scroll.py
@@ -0,0 +1,22 @@
+
+sys.path.append(sys.argv[2])
+import browser
+import common
+
+com = common.General()
+ff = browser.Firefox()
+
+ff.clickBar()
+ff.enterLink(sys.argv[3])
+
+sleep(2)
+wait(Pattern('us_yahoo_com.png').similar(0.80), 60)
+
+icon_loc = wait(Pattern('us_yahoo_com.png').similar(0.80), 60).getTarget()
+x_offset = 0
+y_offset = 150
+inside_window = Location(icon_loc.getX() + x_offset, icon_loc.getY() + y_offset)
+
+mouseMove(inside_window)
+wheel(WHEEL_DOWN, 100)
+wheel(WHEEL_UP, 100)
