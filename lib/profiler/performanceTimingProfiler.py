66acc0a6a7a0abfd9c82a6d8994a9615e10a426b
askeing <askeing@gmail.com>
2016-11-18 17:00:11 +0800

    Fix the timing json codec error issue

--- lib/profiler/performanceTimingProfiler.py
+++ lib/profiler/performanceTimingProfiler.py
@@ -1,5 +1,8 @@
 from ..helper.desktopHelper import DEFAULT_BROWSER_TYPE_FIREFOX
 from base import BaseProfiler
+import json
+import codecs
+import pyperclip
 
 
 class PerformanceTimingProfiler(BaseProfiler):
@@ -14,3 +17,13 @@ class PerformanceTimingProfiler(BaseProfiler):
         else:
             self.sikuli.run_test("test_chrome_timing",
                                  self.env.profile_timing_json_fp)
+
+        data = pyperclip.paste()
+        try:
+            json_data = json.loads(data)
+            data = json.dumps(json_data, indent=4)
+        except:
+            pass
+
+        with codecs.open(self.env.profile_timing_json_fp, "w+", 'utf8') as f:
+            f.write(data)
