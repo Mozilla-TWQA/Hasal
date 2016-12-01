c0e943c15e3fc7d55b37825a2814bf6799642844
askeing <askeing@gmail.com>
2016-12-01 18:31:26 +0800

    Skip click URL bar twice issue

--- lib/sikuli/browser.sikuli/browser.py
+++ lib/sikuli/browser.sikuli/browser.py
@@ -7,6 +7,7 @@ class GeneralBrowser():
     def __init__(self):
         self.os = str(Env.getOS())  # Using Env because of sikuli issue from https://bugs.launchpad.net/sikuli/+bug/1514007
         self.os_version = str(Env.getOSVersion())
+        self.urlbar_loc = None
 
         if self.os.startswith("M"):
             self.control = Key.CMD
@@ -17,13 +18,14 @@ class GeneralBrowser():
         raise NotImplementedError
 
     def enterLink(self, link):
-        urlbar_loc = self.clickBar()
+        if not self.urlbar_loc:
+            self.urlbar_loc = self.clickBar()
         type("a", self.control)
         paste(link)
         type(Key.ENTER)
         x_offset = 0
         y_offset = -35
-        inside_window = Location(urlbar_loc.getX() + x_offset, urlbar_loc.getY() + y_offset)
+        inside_window = Location(self.urlbar_loc.getX() + x_offset, self.urlbar_loc.getY() + y_offset)
         mouseMove(inside_window)
 
     def scroll_down(self, step):
@@ -84,9 +86,9 @@ class Chrome(GeneralBrowser):
         urlbar_pics = ['pics/chrome_urlbar_53.png', 'pics/chrome_urlbar.png']
         for urlbar_pic in urlbar_pics:
             if exists(Pattern(urlbar_pic).similar(0.70)):
-                urlbar_loc = find(Pattern(urlbar_pic).similar(0.70).targetOffset(-40, 0)).getTarget()
+                self.urlbar_loc = find(Pattern(urlbar_pic).similar(0.70).targetOffset(-40, 0)).getTarget()
                 click(Pattern(urlbar_pic).similar(0.70).targetOffset(-40, 0))
-                return urlbar_loc
+                return self.urlbar_loc
 
     # Launch or close web console for developer
     def triggerConsole(self):
@@ -131,9 +133,9 @@ class Firefox(GeneralBrowser):
         urlbar_pics = ['pics/ff_urlbar_black.png', 'pics/ff_urlbar_gray.png']
         for urlbar_pic in urlbar_pics:
             if exists(Pattern(urlbar_pic).similar(0.70)):
-                urlbar_loc = find(Pattern(urlbar_pic).similar(0.70).targetOffset(-80, 0)).getTarget()
+                self.urlbar_loc = find(Pattern(urlbar_pic).similar(0.70).targetOffset(-80, 0)).getTarget()
                 click(Pattern(urlbar_pic).similar(0.70).targetOffset(-80, 0))
-                return urlbar_loc
+                return self.urlbar_loc
 
     # Launch web console for developer
     def triggerConsole(self):
