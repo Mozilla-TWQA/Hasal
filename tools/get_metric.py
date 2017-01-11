294dc51cd11b07a46395aaa0bee7cac399faa9d0
Shako <sho@mozilla.com>
2017-01-11 09:53:39 +0800

    restructure-get-metric-and-fix-firefox-not-in-result

--- tools/get_metric.py
+++ tools/get_metric.py
@@ -11,31 +11,30 @@ class ResultMetricGenerator(object):
         self.output_mode = int(output_mode)
         self.DEFAULT_SHORT_VIEW_MODE = 0
         self.DEFAULT_EASY_PASTE_MODE = 1
+        self.DEFAULT_BROWSER_TYPE_LIST = ['chrome', 'firefox']
 
     def generate_metric(self):
+        DEFAULT_TITLE_LIST = ["CASE NAME", "MEDIAN", "AVG", "STD", "SI", "PSI"]
+        DEFAULT_PRINT_KEY_INDEX = ['med_time', 'avg_time', 'std_dev', 'speed_index', 'perceptual_speed_index']
         with open(self.result_fp) as fh:
             obj_json = json.load(fh)
-            if self.output_mode == self.DEFAULT_EASY_PASTE_MODE:
-                title_format_str = '{s1:<50}\t{s2:<80}\t{s3:<80}'
-                content_format_str = '{s1:<50}\t{s2:<15}\t{s3:<15}\t{s4:<15}\t{s5:<15}\t{s6:<15}\t{s7:<15}\t{s8:<15}\t{s9:<15}\t{s10:<15}\t{s11:<15}'
-            elif self.output_mode == self.DEFAULT_SHORT_VIEW_MODE:
-                content_format_str = '{s1:<50}\t{s2:<15}\t{s3:<15}\t{s4:<15}\t{s5:<15}\t{s6:<15}'
 
+            # generate string format
             if self.output_mode == self.DEFAULT_EASY_PASTE_MODE:
-                print(title_format_str.format(s1="CASE NAME", s2="CHROME", s3="FIREFOX"))
-                print(content_format_str.format(s1="CASE NAME", s2="MEDIAN", s3="AVG", s4="STD", s5="SI", s6="PSI", s7="MEDIAN",
-                                                s8="AVG", s9="STD", s10="SI", s11="PSI"))
-                print(content_format_str.format(s1="##############################", s2="###############", s3="###############",
-                                                s4="###############", s5="###############", s6="###############",
-                                                s7="###############", s8="###############", s9="###############",
-                                                s10="###############", s11="###############"))
+                title_format_str = '{:<50}' + "".join(['\t{:<80}' for i in range(2)])
+                content_format_str = '{:<50}' + "".join(['\t{:<15}' for i in range(len(DEFAULT_PRINT_KEY_INDEX)*2)])
             elif self.output_mode == self.DEFAULT_SHORT_VIEW_MODE:
-                print(
-                    content_format_str.format(s1="CASE NAME", s2="MEDIAN", s3="AVG", s4="STD", s5="SI", s6="PSI"))
-                print(
-                    content_format_str.format(s1="##############################", s2="###############",
-                                              s3="###############",
-                                              s4="###############", s5="###############", s6="###############"))
+                content_format_str = '{:<50}' + "".join(['\t{:<15}' for i in range(len(DEFAULT_PRINT_KEY_INDEX))])
+
+            # print out title bar
+            title_list = copy.deepcopy(DEFAULT_TITLE_LIST)
+            if self.output_mode == self.DEFAULT_EASY_PASTE_MODE:
+                print(title_format_str.format(*("CASE NAME", "CHROME", "FIREFOX")))
+                title_list.extend(title_list[1:])
+            print(content_format_str.format(*tuple(title_list)))
+            sep_list = ["##############################"]
+            sep_list.extend(["###############" for i in range(len(title_list) - 1)])
+            print(content_format_str.format(*tuple(sep_list)))
 
             key_list = copy.deepcopy(obj_json.keys())
             # sort by browser "test_<BROWSER>"_<CASENAME>
@@ -44,10 +43,13 @@ class ResultMetricGenerator(object):
             key_list.sort(key=lambda x: x.split('_', 2)[2])
 
             handle_data = {}
+            browser_type_list = []
+            content_list = []
             if self.output_mode == self.DEFAULT_EASY_PASTE_MODE:
                 for c_name in key_list:
                     case_name = "_".join(c_name.split("_")[2:])
                     browser_type = c_name.split("_")[1].lower()
+                    browser_type_list.append(browser_type)
                     if case_name in handle_data:
                         if browser_type not in handle_data[case_name]:
                             handle_data[case_name][browser_type] = copy.deepcopy(obj_json[c_name])
@@ -55,53 +57,25 @@ class ResultMetricGenerator(object):
                         handle_data[case_name] = {browser_type: obj_json[c_name]}
 
                 for case_name in handle_data:
-                    print(content_format_str.format(s1=case_name,
-                                                    s2=str(handle_data[case_name]["chrome"].get('med_time', 'na')),
-                                                    s3=str(handle_data[case_name]["chrome"].get('avg_time', 'na')),
-                                                    s4=str(handle_data[case_name]["chrome"].get('std_dev', 'na')),
-                                                    s5=str(handle_data[case_name]["chrome"].get('speed_index', 'na')),
-                                                    s6=str(handle_data[case_name]["chrome"].get('perceptual_speed_index',
-                                                                                                'na')),
-                                                    s7=str(
-                                                        handle_data[
-                                                            case_name][
-                                                            "firefox"].get(
-                                                            'med_time',
-                                                            'na')),
-                                                    s8=str(
-                                                        handle_data[
-                                                            case_name][
-                                                            "firefox"].get(
-                                                            'avg_time',
-                                                            'na')),
-                                                    s9=str(
-                                                        handle_data[
-                                                            case_name][
-                                                            "firefox"].get(
-                                                            'std_dev',
-                                                            'na')),
-                                                    s10=str(
-                                                        handle_data[
-                                                            case_name][
-                                                            "firefox"].get(
-                                                            'speed_index',
-                                                            'na')),
-                                                    s11=str(
-                                                        handle_data[
-                                                            case_name][
-                                                            "firefox"].get(
-                                                            'perceptual_speed_index',
-                                                            'na'))
-                                                    ))
+                    print_list = [case_name]
+                    for b_type in self.DEFAULT_BROWSER_TYPE_LIST:
+                        if b_type in handle_data[case_name]:
+                            for key in DEFAULT_PRINT_KEY_INDEX:
+                                print_list.append(str(handle_data[case_name][b_type].get(key, 'na')))
+                        else:
+                            for key in DEFAULT_PRINT_KEY_INDEX:
+                                print_list.append('na')
+                    content_list.append(print_list)
             elif self.output_mode == self.DEFAULT_SHORT_VIEW_MODE:
                 for case_name in key_list:
-                    print(content_format_str.format(s1=case_name,
-                                                    s2=str(obj_json[case_name].get('med_time', 'na')),
-                                                    s3=str(obj_json[case_name].get('avg_time', 'na')),
-                                                    s4=str(obj_json[case_name].get('std_dev', 'na')),
-                                                    s5=str(obj_json[case_name].get('speed_index', 'na')),
-                                                    s6=str(obj_json[case_name].get('perceptual_speed_index', 'na'))
-                                                    ))
+                    print_list = [case_name]
+                    for key in DEFAULT_PRINT_KEY_INDEX:
+                        print_list.append(str(obj_json[case_name].get(key, 'na')))
+                    content_list.append(print_list)
+
+            # print out the result
+            for p_data in content_list:
+                print(content_format_str.format(*tuple(p_data)))
 
     def run(self):
         self.generate_metric()
