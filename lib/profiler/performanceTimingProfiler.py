265665609a2528dca6ea926dded84cdd668adc57
askeing <askeing@gmail.com>
2017-01-12 19:18:17 +0800

    Temporary Solution for applying the settings to profiler, e.g. geckoprofiler

--- lib/profiler/performanceTimingProfiler.py
+++ lib/profiler/performanceTimingProfiler.py
@@ -1,4 +1,3 @@
-from ..helper.desktopHelper import DEFAULT_BROWSER_TYPE_FIREFOX
 from base import BaseProfiler
 import json
 import codecs
@@ -11,7 +10,7 @@ class PerformanceTimingProfiler(BaseProfiler):
         pass
 
     def stop_recording(self, **kwargs):
-        if self.browser_type == DEFAULT_BROWSER_TYPE_FIREFOX:
+        if self.browser_type == self.env.DEFAULT_BROWSER_TYPE_FIREFOX:
             self.sikuli.run_test("test_firefox_timing",
                                  self.env.profile_timing_json_fp)
         else:
