0b0535f397443731b7a28e0791d02ba4d7ebaa45
Walter Chen <ypwalter@gmail.com>
2017-01-16 11:30:48 +0800

    fixed no userId cases

--- scripts/get_trigger.py
+++ scripts/get_trigger.py
@@ -1,17 +1,27 @@
-# This is for Jenkins CI
-# Purpose: Get the trigger and create a file for environment injection
 import os
 import urllib2
 import xml.etree.ElementTree
 
+# initialize variables
+userId=""
+userName=""
+
 # get necessary enviroment variables from os
-job_name = os.environ['JOB_NAME']
-build_num = os.environ['BUILD_NUMBER']
+build_url = os.environ['BUILD_URL']
+full_url = build_url + "api/xml?xpath=//action/cause"
+print full_url
 
 # get the name of whom triggered this build
-data = urllib2.urlopen("http://10.247.120.208:8080/job/" + job_name + "/" + build_num + "/api/xml?xpath=//action/cause/userId").read()
-user = xml.etree.ElementTree.fromstring(data).text
+data = urllib2.urlopen(full_url).read()
+xml_data = xml.etree.ElementTree.fromstring(data)
+
+# if user didn't login, the userId will be empty
+for node in xml_data:
+    if node.tag == "userId":
+        userId = node.text
+    elif node.tag == "userName":
+        userName = node.text
 
 # put it into a file to inject the enviroment
 f = open("temp_env", "w")
-f.write("Trigger=" + user)
+f.write("Trigger=" + userName + "(" + userId + ")")
