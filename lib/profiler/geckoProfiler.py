af8b0970bbae91b46a2ab35119b09ba79a0a1ae3
askeing <askeing@gmail.com>
2017-03-07 15:28:25 +0800

    Run Gecko Profiler with Web Socket

--- lib/profiler/geckoProfiler.py
+++ lib/profiler/geckoProfiler.py
@@ -1,31 +1,50 @@
 from base import BaseProfiler
-import codecs
-import pyperclip
+
+from geckoprofiler_controller.control_server import *
+from geckoprofiler_controller.control_client import *
+from ..common.logConfig import get_logger
+
+logger = get_logger(__name__)
 
 
 class GeckoProfiler(BaseProfiler):
 
     def start_recording(self):
-        pass
+        if self.browser_type == self.env.DEFAULT_BROWSER_TYPE_FIREFOX:
+            # Starting server ...
+            logger.info('Starting Web Socket server for controlling Gecko-Profiler Add-on ...')
+            self.my_server = ServerController()
+            self.my_server.start_server()
 
     def stop_recording(self, **kwargs):
         if self.browser_type == self.env.DEFAULT_BROWSER_TYPE_FIREFOX:
-            self.sikuli.run_test("test_firefox_profile",
-                                 self.env.profile_timing_bin_fp)
-            data = pyperclip.paste()
+            # TODO: create saving folder
+            saving_path = os.path.join(self.env.DEFAULT_PROFILE_OUTPUT_DIR, self.env.output_name)
+            if not os.path.exists(saving_path):
+                os.makedirs(saving_path)
+            saving_url_filename = os.path.join(saving_path, 'URL.txt')
 
-            encoding_list = ["utf8"]
-            encoding = "latin_1"
-            for ec in encoding_list:
-                if ec in self.env.test_name:
-                    encoding = ec
+            # Starting client ...
+            logger.info('Starting Python Gecko-Profiler Controller ...')
+            my_client = ControllerClient(control_server=self.my_server, save_path=saving_path)
+            my_client.connect()
 
-            try:
-                with codecs.open(self.env.profile_timing_bin_fp, "w+", encoding) as f:
-                    f.write(data)
-            except:
-                # if failed, then try to save profile data to utf-8
-                with codecs.open(self.env.profile_timing_bin_fp, "w+", 'utf8') as f:
-                    f.write(data)
-        else:
-            pass
+            # Opening profiling page ...
+            my_client.open_profiling_page()
+
+            # Getting profiling file ...
+            filepath = my_client.get_profiling_file()
+
+            # Getting profiling link ...
+            link = my_client.get_profiling_link()
+
+            logger.info('>> Profiling File: ' + filepath)
+            logger.info('>> Profiling Link: ' + link)
+
+            with open(saving_url_filename, 'w') as f:
+                f.write(link)
+
+            # Close server and disconnect
+            my_client.send_stop_server_command()
+            my_client.disconnect()
+            logger.info('stop server.')
