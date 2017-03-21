69ddc63559ed23ac5f943c0d317133f65ddd3a31
askeing <askeing@gmail.com>
2017-03-21 17:30:07 +0800

    Add dctAnimationInputLatencyGenerator and G-Docs pagedown-input-latency cases

---
+++ lib/generator/dctAnimationInputLatencyGenerator.py
@@ -0,0 +1,120 @@
+import os
+import time
+import copy
+from ..common.imageUtil import generate_crop_data
+from ..common.imageUtil import crop_images
+from ..common.imageUtil import convert_to_dct
+from ..common.imageUtil import compare_with_sample_image_multi_process
+from ..common.visualmetricsWrapper import find_image_viewport
+from ..common.logConfig import get_logger
+
+logger = get_logger(__name__)
+
+
+class DctAnimationInputLatencyGenerator(object):
+
+    SEARCH_TARGET_VIEWPORT = 'viewport'
+    SEARCH_TARGET_ORIGINAL = 'original'
+    SKIP_STATUS_BAR_FRACTION = 1.0
+
+    BROWSER_VISUAL_EVENT_POINTS = {
+        'backward_search': [{'event': 'end', 'search_target': SEARCH_TARGET_VIEWPORT, 'fraction': SKIP_STATUS_BAR_FRACTION},
+                            {'event': 'start', 'search_target': SEARCH_TARGET_ORIGINAL, 'fraction': SKIP_STATUS_BAR_FRACTION}]
+    }
+
+    @staticmethod
+    def generate_sample_result(input_generator_name, input_sample_dict, input_sample_index):
+        current_sample_data = copy.deepcopy(input_sample_dict)
+        input_sample_data = current_sample_data[input_sample_index]
+        sample_dct_obj = convert_to_dct(input_sample_data['fp'])
+        return_result = {input_generator_name: {'dct': sample_dct_obj, 'crop_data': {}}}
+
+        # crop sample data area
+        # generate viewport crop area
+        if DctAnimationInputLatencyGenerator.SEARCH_TARGET_VIEWPORT in input_sample_data:
+            return_result[input_generator_name]['crop_data'][DctAnimationInputLatencyGenerator.SEARCH_TARGET_VIEWPORT] = input_sample_data[DctAnimationInputLatencyGenerator.SEARCH_TARGET_VIEWPORT]
+        else:
+            viewport_value = find_image_viewport(input_sample_data['fp'])
+            return_result[input_generator_name]['crop_data'][DctAnimationInputLatencyGenerator.SEARCH_TARGET_VIEWPORT] = viewport_value
+            return_result[DctAnimationInputLatencyGenerator.SEARCH_TARGET_VIEWPORT] = viewport_value
+
+        # generate crop data
+        if input_generator_name not in input_sample_dict[1]:
+            crop_data_dict = generate_crop_data([input_sample_data], return_result[input_generator_name]['crop_data'])
+        else:
+            crop_data_dict = generate_crop_data([input_sample_data], input_sample_dict[1][input_generator_name]['crop_data'])
+
+        # crop images
+        crop_images(crop_data_dict)
+
+        if input_sample_index == 1:
+            return_result[input_generator_name]['event_tags'] = {}
+        elif input_sample_index == 2:
+            # tag event to sample
+            return_result[input_generator_name]['event_tags'] = {}
+            for event_obj in DctAnimationInputLatencyGenerator.BROWSER_VISUAL_EVENT_POINTS['backward_search']:
+                if event_obj['search_target'] == DctAnimationInputLatencyGenerator.SEARCH_TARGET_ORIGINAL:
+                    return_result[input_generator_name]['event_tags'][event_obj['event']] = sample_dct_obj
+                else:
+                    search_target_fp = crop_data_dict[event_obj['search_target']]['fp_list'][0]['output_fp']
+                    return_result[input_generator_name]['event_tags'][event_obj['event']] = convert_to_dct(search_target_fp, event_obj['fraction'])
+
+        return return_result
+
+    def crop_images_based_on_samplefiles(self, input_data):
+        # compare source image list
+        input_image_list = copy.deepcopy(input_data['converter_result'])
+
+        # generate image list
+        image_list = []
+        for image_fn in input_data['converter_result']:
+            image_list.append(input_data['converter_result'][image_fn])
+
+        # generate crop data for all images
+        crop_data_dict = generate_crop_data(image_list,
+                                            input_data['sample_result'][1][self.__class__.__name__]['crop_data'])
+
+        # crop images
+        start_time = time.time()
+        crop_images(crop_data_dict)
+        last_end = time.time()
+        elapsed_time = last_end - start_time
+        logger.debug("Crop Image Time Elapsed: [%s]" % elapsed_time)
+
+        # merge crop data and convert data
+        for crop_target_name in crop_data_dict:
+            for crop_img_obj in crop_data_dict[crop_target_name]['fp_list']:
+                image_fn = os.path.basename(crop_img_obj['input_fp'])
+                input_image_list[image_fn][crop_target_name] = crop_img_obj['output_fp']
+
+        # merge crop data and convert data
+        for image_fn in input_image_list:
+            input_image_list[image_fn][self.SEARCH_TARGET_ORIGINAL] = input_image_list[image_fn]['fp']
+
+        return input_image_list
+
+    def generate_result(self, input_data, input_global_result):
+        """
+
+        @param input_data:
+        @return:
+        """
+        compare_result = {}
+
+        input_image_list = self.crop_images_based_on_samplefiles(input_data)
+        compare_result['merged_crop_image_list'] = input_image_list
+
+        compare_setting = {
+            'default_fps': input_data['default_fps'],
+            'event_points': self.BROWSER_VISUAL_EVENT_POINTS,
+            'generator_name': self.__class__.__name__,
+            'skip_status_bar_fraction': self.SKIP_STATUS_BAR_FRACTION,
+            'exec_timestamp_list': input_data['exec_timestamp_list'],
+            'threshold': 0.005,
+            'search_margin': 1
+        }
+        compare_result['running_time_result'] = compare_with_sample_image_multi_process(input_data['sample_result'],
+                                                                                        input_image_list,
+                                                                                        compare_setting)
+
+        return compare_result
---
+++ tests/regression/gdoc/test_firefox_gdoc_pagedown_input_latency.sikuli/test_firefox_gdoc_pagedown_input_latency.py
@@ -0,0 +1,35 @@
+# if you are putting your test script folders under {git project folder}/tests/, it will work fine.
+# otherwise, you either add it to system path before you run or hard coded it in here.
+sys.path.append(sys.argv[2])
+import os
+import gdoc
+import shutil
+import browser
+import time
+import json
+
+ff = browser.Firefox()
+gd = gdoc.gDoc()
+ff.clickBar()
+ff.enterLink(sys.argv[3])
+gd.wait_for_loaded()
+
+setAutoWaitTimeout(10)
+sample2_fp = os.path.join(sys.argv[4], sys.argv[5].replace('sample_1', 'sample_2'))
+
+sleep(2)
+capture_width = int(sys.argv[6])
+capture_height = int(sys.argv[7])
+
+t1 = time.time()
+capimg2 = capture(0, 0, capture_width, capture_height)
+type(Key.PAGE_DOWN)
+sleep(1)
+t2 = time.time()
+with open(sys.argv[8], "r+") as fh:
+    timestamp = json.load(fh)
+    timestamp['t1'] = t1
+    timestamp['t2'] = t2
+    fh.seek(0)
+    fh.write(json.dumps(timestamp))
+shutil.move(capimg2, sample2_fp.replace(os.path.splitext(sample2_fp)[1], '.png'))
