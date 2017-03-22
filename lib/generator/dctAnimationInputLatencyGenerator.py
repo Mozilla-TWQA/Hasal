6d8ed8cb501518753b7ba0c63bfac5d74d80a478
askeing <askeing@gmail.com>
2017-03-22 18:20:57 +0800

    Update the threshold for different platform - the threshold on Windows and Ubuntu are different.

--- lib/generator/dctAnimationInputLatencyGenerator.py
+++ lib/generator/dctAnimationInputLatencyGenerator.py
@@ -104,13 +104,19 @@ class DctAnimationInputLatencyGenerator(object):
         input_image_list = self.crop_images_based_on_samplefiles(input_data)
         compare_result['merged_crop_image_list'] = input_image_list
 
+        # TODO: the threshold on Windows and Ubuntu are different.
+        diff_threshold = 0.005
+        import sys
+        if sys.platform == 'win32':
+            diff_threshold = 0.01
+
         compare_setting = {
             'default_fps': input_data['default_fps'],
             'event_points': self.BROWSER_VISUAL_EVENT_POINTS,
             'generator_name': self.__class__.__name__,
             'skip_status_bar_fraction': self.SKIP_STATUS_BAR_FRACTION,
             'exec_timestamp_list': input_data['exec_timestamp_list'],
-            'threshold': 0.005,
+            'threshold': diff_threshold,
             'search_margin': 1
         }
         compare_result['running_time_result'] = compare_with_sample_image_multi_process(input_data['sample_result'],
