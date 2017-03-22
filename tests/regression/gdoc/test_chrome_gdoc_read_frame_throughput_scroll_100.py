48b4317a108d0d201d8ab172d9a8950f859771a0
Mike Lien <mlien@mozilla.com>
2017-03-22 17:04:02 +0800

    add gdoc scroll cases

---
+++ tests/regression/gdoc/test_chrome_gdoc_read_frame_throughput_scroll_100.py
@@ -0,0 +1,16 @@
+from lib.perfBaseTest import PerfBaseTest
+
+
+class TestSikuli(PerfBaseTest):
+
+    def setUp(self):
+        self.set_variable(test_target=self.env.TEST_TARGET_ID_3_PAGE_CONTENT_WITH_TXT_TABLE_IMAGE)
+        super(TestSikuli, self).setUp()
+
+    def test_chrome_gdoc_read_frame_throughput_scroll_100(self):
+        self.round_status = self.sikuli.run_test(self.env.test_name, self.env.output_name, test_target=self.test_url,
+                                                 script_dp=self.env.test_script_py_dp,
+                                                 args_list=[self.env.img_sample_dp, self.env.img_output_sample_1_fn,
+                                                            self.env.DEFAULT_VIDEO_RECORDING_WIDTH,
+                                                            self.env.DEFAULT_VIDEO_RECORDING_HEIGHT,
+                                                            self.env.DEFAULT_TIMESTAMP])
---
+++ tests/regression/gdoc/test_firefox_gdoc_read_frame_throughput_scroll_100.sikuli/test_firefox_gdoc_read_frame_throughput_scroll_100.py
@@ -0,0 +1,38 @@
+# if you are putting your test script folders under {git project folder}/tests/, it will work fine.
+# otherwise, you either add it to system path before you run or hard coded it in here.
+sys.path.append(sys.argv[2])
+import browser
+import common
+import gdoc
+import shutil
+import time
+import os
+
+com = common.General()
+ff = browser.Firefox()
+gd = gdoc.gDoc()
+
+ff.clickBar()
+ff.enterLink(sys.argv[3])
+sleep(5)
+gd.wait_for_loaded()
+
+setAutoWaitTimeout(10)
+sample1_fp = os.path.join(sys.argv[4], sys.argv[5])
+sample2_fp = os.path.join(sys.argv[4], sys.argv[5].replace('sample_1', 'sample_2'))
+os.remove(sample1_fp)
+
+sleep(2)
+capture_width = int(sys.argv[6])
+capture_height = int(sys.argv[7])
+
+t1 = time.time()
+capimg1 = capture(0, 0, capture_width, capture_height)
+wheel(Pattern("pics/doc_content_left_top_page_region.png").similar(0.85), WHEEL_DOWN, 100)
+# type(Key.PAGE_DOWN)
+sleep(1)
+t2 = time.time()
+capimg2 = capture(0, 0, capture_width, capture_height)
+com.updateJson({'t1': t1, 't2': t2}, sys.argv[8])
+shutil.move(capimg1, sample1_fp.replace(os.path.splitext(sample1_fp)[1], '.png'))
+shutil.move(capimg2, sample2_fp.replace(os.path.splitext(sample1_fp)[1], '.png'))
