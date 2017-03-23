af63e719c6c29fab828317c70f857f1120afb1a8
Bobby Chien <bobby.chien@gmail.com>
2017-03-23 15:21:35 +0800

    Integrate pandas and matplotlib

--- tools/InputLatency.py
+++ tools/InputLatency.py
@@ -1,28 +1,37 @@
-from IPython.display import display
-from ipywidgets import *
+#!/usr/bin/env python
+
 import pandas as pd
 import numpy as np
-import json 
+import json
 import matplotlib.pyplot as plt
 import argparse
 from argparse import ArgumentDefaultsHelpFormatter
 
 
 class InputLatencyGenerator(object):
-    
+
+    """
+    class InputLatencyGenerator
+    """
+
     def __init__(self, input_result_fp, output_mode=0):
+        """
+        __init__
+        """
         self.result_fp = input_result_fp
         self.output_mode = int(output_mode)
         self.DEFAULT_SHORT_VIEW_MODE = 0
         self.DEFAULT_EASY_PASTE_MODE = 1
 
     def generate_metric(self):
-        DEFAULT_TITLE_LIST = ["CASE NAME", "MEDIAN", "AVG", "STD", "SI", "PSI"]
         DEFAULT_PRINT_KEY_INDEX = ['avg_time',
-                                    'max_time', 
-                                    'med_time',
-                                    'min_time',
-                                    'std_dev']
+                                   'max_time',
+                                   'med_time',
+                                   'min_time',
+                                   'std_dev']
+        """
+        generate_metric
+        """
 
         with open(self.result_fp) as data_file:
             # Setup pandas display options
@@ -34,23 +43,35 @@ class InputLatencyGenerator(object):
             d = pd.DataFrame(data)
 
             # Analyze input latency
-            runtime = pd.DataFrame([pd.DataFrame(d[c]['time_list'])['run_time'] for c in d]).T
+            runtime = pd.DataFrame(
+                [pd.DataFrame(d[c]['time_list'])['run_time'] for c in d]).T
             runtime.columns = d.columns
-            print '===== Input Latency Raw data===='
+            print '===== Input Latency Summary ===='
+            print d.loc[DEFAULT_PRINT_KEY_INDEX, :]
+            print '\n\n===== Input Latency Raw runtime ===='
             print runtime
-            print '\n\n===== Input Latency Percentile===='
+            print '\n\n===== Input Latency runtime Percentile===='
             print runtime.quantile([0.01, 0.99], interpolation='nearest')
-            print '\n\n===== Input Latency Summary Table===='
+            print '\n\n===== Input Latency runtime Table===='
             print runtime.describe()
-            print '\n\n===== Input Latency Plots===='            
-            runtime.plot.hist(bins=20, histtype='bar', color=['green','orange'])
+            print '\n\n===== Input Latency runtime Plots===='
+            runtime.plot.hist(bins=20, histtype='bar',
+                              color=['green', 'orange', 'red', 'blue'])
             runtime.plot.box()
-            #plt.show()
+            plt.show()
 
     def run(self):
+        """
+        run()
+        """
         self.generate_metric()
 
+
 def main():
+    """ 
+    function main() as entry point 
+    """
+
     arg_parser = argparse.ArgumentParser(description='Input Latency Report',
                                          formatter_class=ArgumentDefaultsHelpFormatter)
     arg_parser.add_argument('-i', action='store', dest='input_result_fp', default=None,
@@ -58,6 +79,7 @@ def main():
     args = arg_parser.parse_args()
     run_obj = InputLatencyGenerator(args.input_result_fp)
     run_obj.run()
+
 
 if __name__ == '__main__':
     main()
