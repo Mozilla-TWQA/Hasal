b2aaef2d9e2076dfa41acd013b45ad9097646dd3
Walter Chen <ypwalter@gmail.com>
2017-03-23 11:32:31 +0800

    making changes to align new timestamp and windows movement

--- lib/webdriverBaseTest.py
+++ lib/webdriverBaseTest.py
@@ -1,4 +1,5 @@
 import os
+import json
 import time
 import baseTest
 import lib.webdriver as wd
@@ -18,9 +19,6 @@ class WebdriverBaseTest(baseTest.BaseTest):
     def setUp(self):
         super(WebdriverBaseTest, self).setUp()
 
-        # minimize all windows
-        desktopHelper.minimize_window()
-
         # launch browser
         self.browser_obj, self.profile_dir_path = \
             desktopHelper.launch_browser(self.browser_type, env=self.env, type='webdriver',
@@ -33,8 +31,10 @@ class WebdriverBaseTest(baseTest.BaseTest):
         self.profilers = Profilers(self.env, self.browser_type, self.wd)
         self.profilers.start_profiling(self.env.firefox_settings_extensions)
 
-        # Record timestamp t1
-        self.exec_timestamp_list.append(self.profilers.get_t1_time())
+        # Record initial timestamp
+        with open(self.env.DEFAULT_TIMESTAMP, "w") as fh:
+            timestamp = {self.env.INITIAL_TIMESTAMP_NAME: str(time.time())}
+            json.dump(timestamp, fh)
 
         # wait browser ready / must do after launching browser and starting of video recording
         self.get_browser_done()
@@ -47,17 +47,26 @@ class WebdriverBaseTest(baseTest.BaseTest):
                                            self.env.img_output_sample_1_fn)
         time.sleep(2)
 
-        # Record timestamp t2
-        self.exec_timestamp_list.append(time.time())
+        # Record timestamp t1
+        with open(self.env.DEFAULT_TIMESTAMP, "r+") as fh:
+            timestamp = json.load(fh)
+            timestamp['t1'] = time.time()
+            fh.seek(0)
+            fh.write(json.dumps(timestamp))
 
     def tearDown(self):
 
-        # Record timestamp t3
-        self.exec_timestamp_list.append(time.time())
+        # Record timestamp t2
+        with open(self.env.DEFAULT_TIMESTAMP, "r+") as fh:
+            timestamp = json.load(fh)
+            if 't2' not in timestamp:
+                timestamp['t2'] = time.time()
+                fh.seek(0)
+                fh.write(json.dumps(timestamp))
 
         # capture 2nd snapshot
         time.sleep(5)
-        if self.env.PROFILER_FLAG_AVCONV in self.env.firefox_settings_extensions:
+        if not int(os.getenv("ENABLE_WAVEFORM")) and self.env.PROFILER_FLAG_AVCONV in self.env.firefox_settings_extensions:
             if self.env.firefox_settings_extensions[self.env.PROFILER_FLAG_AVCONV]['enable'] is True:
                 videoHelper.capture_screen(self.env, self.env.video_output_sample_2_fp, self.env.img_sample_dp,
                                            self.env.img_output_sample_2_fn)
