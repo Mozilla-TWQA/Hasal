de28c1cdaef5bb41a86a8263d4ad743bad28e7c2
Walter Chen <ypwalter@gmail.com>
2017-04-12 16:56:11 +0800

    sikuli_status was out-of-date. please do use round_status as returned status next time please

--- tests/regression/gdoc/test_firefox_gdoc_ail_type_0.py
+++ tests/regression/gdoc/test_firefox_gdoc_ail_type_0.py
@@ -8,7 +8,7 @@ class TestSikuli(PerfBaseTest):
         super(TestSikuli, self).setUp()
 
     def test_firefox_gdoc_ail_type_0(self):
-        self.sikuli_status = self.sikuli.run_test(self.env.test_name, self.env.output_name, test_target=self.test_url,
+        self.round_status = self.sikuli.run_test(self.env.test_name, self.env.output_name, test_target=self.test_url,
                                                   script_dp=self.env.test_script_py_dp,
                                                   args_list=[self.env.img_sample_dp, self.env.img_output_sample_1_fn,
                                                              self.env.DEFAULT_VIDEO_RECORDING_WIDTH,
