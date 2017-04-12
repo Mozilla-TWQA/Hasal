71f267a5987d9777376d9bbfcc61258b4501df4f
Walter Chen <ypwalter@gmail.com>
2017-04-12 17:24:45 +0800

    fix pep8 indent issue

--- tests/regression/gsheet/test_firefox_gsheet_ail_type_0.py
+++ tests/regression/gsheet/test_firefox_gsheet_ail_type_0.py
@@ -9,8 +9,8 @@ class TestSikuli(PerfBaseTest):
 
     def test_firefox_gsheet_ail_type_0(self):
         self.round_status = self.sikuli.run_test(self.env.test_name, self.env.output_name, test_target=self.test_url,
-                                                  script_dp=self.env.test_script_py_dp,
-                                                  args_list=[self.env.img_sample_dp, self.env.img_output_sample_1_fn,
-                                                             self.env.DEFAULT_VIDEO_RECORDING_WIDTH,
-                                                             self.env.DEFAULT_VIDEO_RECORDING_HEIGHT,
-                                                             self.env.DEFAULT_TIMESTAMP])
+                                                 script_dp=self.env.test_script_py_dp,
+                                                 args_list=[self.env.img_sample_dp, self.env.img_output_sample_1_fn,
+                                                            self.env.DEFAULT_VIDEO_RECORDING_WIDTH,
+                                                            self.env.DEFAULT_VIDEO_RECORDING_HEIGHT,
+                                                            self.env.DEFAULT_TIMESTAMP])
