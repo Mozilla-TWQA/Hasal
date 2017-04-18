67ca4a017af67cc9fc7fd0eb3a44f6caef95cbd1
Mike Lien <mlien@mozilla.com>
2017-04-18 17:19:34 +0800

    fix config syntax error and move load config to init 1. add comma in global config to fix error 2. move load config to init to avoid case error 3. change method call from wrapper

--- lib/baseTest.py
+++ lib/baseTest.py
@@ -25,6 +25,9 @@ class BaseTest(unittest.TestCase):
         # Init environment variables
         self.env = Environment(self._testMethodName, self._testMethodDoc)
 
+        # load all settings into self object
+        self.load_configs()
+
         # Get Terminal Window Object here when it still active
         if sys.platform == 'darwin':
             terminal_title = ['Terminal.app', 'iTerm.app']
@@ -149,9 +152,6 @@ class BaseTest(unittest.TestCase):
             setattr(self, config_variable_name, config_value)
 
     def setUp(self):
-
-        # load all settings into self object
-        self.load_configs()
 
         # Original profiler list was substitute by self.env.firefox_settings_extensions
         # We set the original profiler path variable in the variable
