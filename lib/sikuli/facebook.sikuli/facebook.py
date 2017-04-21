0fd9bccbfbffcb218e645b476065d8d382b4260e
Mike Lien <mlien@mozilla.com>
2017-04-21 11:07:22 +0800

    add fb open chat tab case

--- lib/sikuli/facebook.sikuli/facebook.py
+++ lib/sikuli/facebook.sikuli/facebook.py
@@ -44,6 +44,8 @@ class facebook():
         self.save_button = Pattern("pics/facebook_save_button.png").similar(0.70)
         self.club_post_menu_delete = Pattern("pics/facebook_club_post_menu_delete.png").similar(0.70)
         self.messenger_header = Pattern("pics/facebook_messenger_header.png").similar(0.70)
+        self.right_panel_contact = Pattern("pics/facebook_contact.png").similar(0.70)
+        self.chat_tab_close_button = Pattern("pics/facebook_chat_tab_close_button.png").similar(0.70)
 
         self.sampleImg1 = os.path.join(os.path.dirname(os.path.realpath(__file__)), "content", "sample_1.jpg")
 
