a921edbe551691f369feb02b5976d23b334c2a95
askeing <askeing@gmail.com>
2017-04-26 16:31:12 +0800

    Supporting Chrome Profile on Mac OSX

--- lib/helper/chromeProfileCreator.py
+++ lib/helper/chromeProfileCreator.py
@@ -1,10 +1,13 @@
 import os
 import sys
 import time
+import psutil
 import shutil
 import tempfile
+import subprocess
 from lib.common.logConfig import get_logger
 from lib.common.pyDriveUtil import PyDriveUtil
+from lib.helper.desktopHelper import close_browser
 
 logger = get_logger(__name__)
 
@@ -12,12 +15,13 @@ logger = get_logger(__name__)
 class ChromeProfileCreator(object):
     def __init__(self, chrome_profile_path=''):
         self.browser_type = "chrome"
-        self.process_name = "chrome.exe"
         self.current_platform_name = sys.platform
         if self.current_platform_name == 'darwin':
             self.chrome_cmd = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+            self.process_name = "Google Chrome"
         elif self.current_platform_name == "linux2":
             self.chrome_cmd = 'google-chrome'
+            self.process_name = "chrome.exe"
         else:
             self.chrome_cmd = 'chrome'
         self._chrome_profile_path = chrome_profile_path
@@ -35,8 +39,35 @@ class ChromeProfileCreator(object):
         tmp_profile_dir = tempfile.mkdtemp(prefix='chromeprofile_')
         logger.info('Creating Profile: {}'.format(tmp_profile_dir))
         self._chrome_profile_path = tmp_profile_dir
-        # TODO: need to fix mac OS always on background and cannot return issue in the future
-        os.system('{} --no-startup-window --user-data-dir={}'.format(self.chrome_cmd, tmp_profile_dir))
+        if self.current_platform_name == 'darwin':
+            launch_cmd = [self.chrome_cmd, '--user-data-dir={}'.format(tmp_profile_dir)]
+            subprocess.Popen(launch_cmd)
+            # wait for folder creation
+            for _ in range(10):
+                chrome_profile_default_folder = os.path.join(self._chrome_profile_path, 'Default')
+                if os.path.exists(chrome_profile_default_folder):
+                    logger.debug('Profile default folder is created.')
+                    time.sleep(0.5)
+                    break
+                else:
+                    time.sleep(0.5)
+            # close browser
+            close_browser(self.browser_type)
+            # wait for process finish
+            for _ in range(10):
+                p_exist = False
+                for p in psutil.process_iter():
+                    if self.process_name.lower() in p.name().lower():
+                        p_exist = True
+                        break
+                if p_exist:
+                    time.sleep(0.5)
+                else:
+                    break
+
+        else:
+            cmd_str = '{} --no-startup-window --user-data-dir={}'.format(self.chrome_cmd, tmp_profile_dir)
+            os.system(cmd_str)
         logger.info('Creating Profile success: {}'.format(self._chrome_profile_path))
         return self._chrome_profile_path
 
