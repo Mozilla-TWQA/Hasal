88fbb25404257242838c6a86a45b921b1f2cea2d
askeing <askeing@gmail.com>
2017-04-28 14:52:50 +0800

    Adding the comments of compose new mail via keyboard - This case have to enable "Keyboard Shortcuts" feature

--- tests/regression/gmail/test_firefox_gmail_ail_compose_new_mail_via_keyboard.py
+++ tests/regression/gmail/test_firefox_gmail_ail_compose_new_mail_via_keyboard.py
@@ -7,6 +7,15 @@ class TestSikuli(PerfBaseTest):
         super(TestSikuli, self).setUp()
 
     def test_firefox_gmail_ail_compose_new_mail_via_keyboard(self):
+        """
+        This performance case should open the keyboard shortcuts feature.
+        Steps:
+            1. open GMail
+            2. Click the Settings Gear on top-right side
+            3. Go to General tab
+            4. Enable Keyboard Shortcuts
+            5. Save Changes
+        """
         self.test_url = self.global_config['gsuite']['gmail']['test-url']
 
         self.round_status = self.sikuli.run_test(self.env.test_name,
