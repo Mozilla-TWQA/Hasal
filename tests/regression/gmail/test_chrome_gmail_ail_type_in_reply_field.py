87fedb4b5a0c477a380d8a7df0d32250c19e2499
askeing <askeing@gmail.com>
2017-04-28 17:33:55 +0800

    Modify the threshold of reply_mail

--- tests/regression/gmail/test_chrome_gmail_ail_type_in_reply_field.py
+++ tests/regression/gmail/test_chrome_gmail_ail_type_in_reply_field.py
@@ -5,6 +5,9 @@ class TestSikuli(PerfBaseTest):
 
     def setUp(self):
         super(TestSikuli, self).setUp()
+        self.set_configs(self.config_name.INDEX,
+                         self.extract_platform_dep_settings({'win32': {'7': {'compare-threshold': 0.003},
+                                                                       '10': {'compare-threshold': 0.003}}}))
 
     def test_chrome_gmail_ail_type_in_reply_field(self):
         self.test_url = self.global_config['gsuite']['gmail']['test-url']
