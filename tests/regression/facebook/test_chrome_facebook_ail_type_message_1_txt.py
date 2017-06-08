b42dd65332b5f0bac312e0e88587b8cfa346302f
Walter Chen <ypwalter@gmail.com>
2017-06-08 14:18:52 +0800

    refactor 3 facebook cases for customized region
    1. composerbox type ail
    2. message type ail
    3. comment type ail

--- tests/regression/facebook/test_chrome_facebook_ail_type_message_1_txt.py
+++ tests/regression/facebook/test_chrome_facebook_ail_type_message_1_txt.py
@@ -5,11 +5,17 @@ class TestSikuli(PerfBaseTest):
 
     def setUp(self):
         super(TestSikuli, self).setUp()
+        self.set_configs(self.config_name.INDEX,
+                         self.extract_platform_dep_settings({"win32": {"7": {"compare-threshold": 0.015},
+                                                                       '10': {'compare-threshold': 0.02}}}))
 
     def test_chrome_facebook_ail_type_message_1_txt(self):
-        self.round_status = self.sikuli.run_test(self.env.test_name, self.env.output_name, test_target=self.env.TEST_FB_MESSAGES,
+        self.round_status = self.sikuli.run_test(self.env.test_name,
+                                                 self.env.output_name,
+                                                 test_target=self.env.TEST_FB_MESSAGES,
                                                  script_dp=self.env.test_script_py_dp,
-                                                 args_list=[self.env.img_sample_dp, self.env.img_output_sample_1_fn,
+                                                 args_list=[self.env.img_sample_dp,
+                                                            self.env.img_output_sample_1_fn,
                                                             self.exec_config['video-recording-width'],
                                                             self.exec_config['video-recording-height'],
                                                             self.env.DEFAULT_TIMESTAMP])
