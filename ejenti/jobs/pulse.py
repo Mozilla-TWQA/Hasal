90d3189b1287f3d66fa52d2391a98b2d78cf540d
askeing <askeing@gmail.com>
2017-08-07 15:09:02 +0800

    Adding Pulse "mgt" topic for sending Agent response to Trigger

--- ejenti/jobs/pulse.py
+++ ejenti/jobs/pulse.py
@@ -1,9 +1,14 @@
 import sys
+import socket
 import pickle
 import logging
 import platform
+
+from mozillapulse.messages.base import GenericMessage
+
 # The job will be imported by ejenti. Top level will be `ejenti`, not `hasal` or `ejenti.jobs`.
 from pulse_modules.hasal_consumer import HasalConsumer  # NOQA
+from pulse_modules.hasal_publisher import HasalPublisher  # NOQA
 
 
 PULSE_KEY_TASK = 'task'
@@ -86,14 +91,22 @@ def listen_pulse(**kwargs):
         queue_type_async: async_queue
     }
 
-    # define the callback
     def got_msg(body, message):
-        # handle the message
-        # ack then broker will remove this message from queue
+        """
+        handle the message
+        ack then broker will remove this message from queue
+        """
         message.ack()
 
         data_payload = body.get('payload')
 
+        # handle response info to Trigger
+        try:
+            response_agent_info(data_payload)
+        except Exception as e:
+            logging.error(e)
+
+        # logging debug information
         debug_message = ''
         for debug_key in [key for key in data_payload.keys() if key.startswith(PULSE_KEY_DEBUG_PREFIX)]:
             debug_message += '{key}: {value}\n'.format(key=debug_key, value=data_payload.get(debug_key, ''))
@@ -101,9 +114,11 @@ def listen_pulse(**kwargs):
             logging.debug('### Pulse Got MSG ###\n{line}\n{dbg_msg}{line}'.format(dbg_msg=debug_message,
                                                                                   line='-' * 20))
 
+        # get MetaTask
         meta_task = data_payload.get(PULSE_KEY_TASK)
         meta_task_object = pickle.loads(meta_task)
 
+        # Handle command into specify queue
         task_command_key = meta_task_object.command_key
         task_queue_type = meta_task_object.queue_type
         logging.debug('Task Command Key: {}, and Queue Type: {}'.format(task_command_key, task_queue_type))
@@ -119,6 +134,59 @@ def listen_pulse(**kwargs):
         logging.debug('Push task into [{}] queue with command name {}.'.format(task_queue_type,
                                                                                task_command_key))
 
+    def response_agent_info(data_payload):
+        """
+        Return back the received MetaTask UID and agent information to Pulse "mgt" topic channel.
+        """
+        # the debug information key name
+        DEBUG_QUEUE_TYPE = 'debug_queue_type'
+        DEBUG_COMMAND_NAME = 'debug_command_name'
+        DEBUG_OVERWRITE_COMMAND_CONFIGS = 'debug_overwrite_command_configs'
+        DEBUG_UID = 'debug_UID'
+        # define mgt keys
+        PULSE_MGT_TOPIC = 'mgt'
+        PULSE_MGT_OBJECT_KEY = 'message'
+        PULSE_MGT_OBJECT_TASK_UID = 'task_uid'
+        PULSE_MGT_OBJECT_HOSTNAME = 'hostname'
+        PULSE_MGT_OBJECT_IP = 'ip'
+        PULSE_MGT_OBJECT_TOPIC = 'topic'
+        PULSE_MGT_OBJECT_PLATFORM = 'platform'
+        PULSE_MGT_OBJECT_CMD_NAME = 'cmd_name'
+        PULSE_MGT_OBJECT_CMD_CFG = 'cmd_configs'
+        PULSE_MGT_OBJECT_QUEUE_TYPE = 'queue_type'
+        # getting the agent information
+        ret_uid = data_payload.get(DEBUG_UID, '')
+        ret_hostname = socket.gethostname()
+        ret_ip = socket.gethostbyname(socket.gethostname())
+        ret_topic = get_topic()
+        ret_platform = sys.platform
+        ret_cmd_name = data_payload.get(DEBUG_COMMAND_NAME, '')
+        ret_cmd_cfg = data_payload.get(DEBUG_OVERWRITE_COMMAND_CONFIGS, '')
+        ret_queue_type = data_payload.get(DEBUG_QUEUE_TYPE, '')
+        # prepare the sending data
+        data = {
+            PULSE_MGT_OBJECT_TASK_UID: ret_uid,
+            PULSE_MGT_OBJECT_HOSTNAME: ret_hostname,
+            PULSE_MGT_OBJECT_IP: ret_ip,
+            PULSE_MGT_OBJECT_TOPIC: ret_topic,
+            PULSE_MGT_OBJECT_PLATFORM: ret_platform,
+            PULSE_MGT_OBJECT_CMD_NAME: ret_cmd_name,
+            PULSE_MGT_OBJECT_CMD_CFG: ret_cmd_cfg,
+            PULSE_MGT_OBJECT_QUEUE_TYPE: ret_queue_type
+        }
+        # make publisher
+        p = HasalPublisher(user=username, password=password)
+        # prepare message
+        mymessage = GenericMessage()
+        # setup topic
+        mymessage.routing_parts.append(PULSE_MGT_TOPIC)
+        mymessage.set_data(PULSE_MGT_OBJECT_KEY, data)
+        # send
+        p.publish(mymessage)
+        # disconnect
+        p.disconnect()
+
+    # create consumer
     c = HasalConsumer(user=username, password=password, applabel=consumer_label)
     c.configure(topic=topic, callback=got_msg)
 
