1dc63da45e1134a62811bb64708472a228abd847
askeing <askeing@gmail.com>
2017-08-18 14:43:31 +0800

    Adding FirefoxNightly support in bootstrap

--- bootstrap-mac.sh
+++ bootstrap-mac.sh
@@ -215,7 +215,7 @@ if [[ ${RET_SUCCESS} == ${CHECK_CV2_RET} ]] && [[ ${RET_SUCCESS} == ${CHECK_SYS_
         func_log "[NOTE] Skip download Certificates into Hasal's folder ..."
     else
         func_log "[NOTE] Please login your Mozilla account, and download Certificates into Hasal's folder ..."
-        open -a firefox -g http://goo.gl/ALcw0B
+        open -a firefox -g http://goo.gl/ALcw0B || open -a firefoxnightly -g http://goo.gl/ALcw0B
     fi
     func_log ""
 
