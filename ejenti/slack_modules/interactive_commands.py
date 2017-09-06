bbd3c7f5d1735a6f4aaf890b4bcd0c7a158ac8d6
askeing <askeing@gmail.com>
2017-09-06 17:57:43 +0800

    Adding Slack Interactive Commands support - slack-disk-usage

---
+++ ejenti/slack_modules/interactive_commands.py
@@ -0,0 +1,31 @@
+import os
+import psutil
+
+
+def cmd_disk_usage(sending_queue, cmd_configs={}):
+    """
+    [Slack Command]
+    Return the agent disk usage.
+    """
+    disk_usage = psutil.disk_usage(os.path.abspath(os.sep))
+    current_percent = disk_usage.percent
+    msg = 'Current disk usage is *{percent}%* ({used}/{total} bytes).'.format(percent=current_percent,
+                                                                              used=disk_usage.used,
+                                                                              total=disk_usage.total)
+
+    msg_obj = _generate_slack_sending_message(msg)
+    sending_queue.put(msg_obj)
+    pass
+
+
+def _generate_slack_sending_message(message, channel='mgt'):
+    """
+    Return an object which contain the sending message which can be put into slack_sending_queue.
+    """
+    if channel not in ['mgt', 'election']:
+        channel = 'mgt'
+    ret_obj = {
+        'message': message,
+        'channel': channel
+    }
+    return ret_obj
