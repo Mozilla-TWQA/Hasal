3ec0c5b4b3d844de0746a501f873aa91519efbb3
Mike Lien <mlien@mozilla.com>
2017-09-11 16:56:39 +0800

    media request time to first paint

---
+++ tests/request/media/test_firefox_media_ail_click_playback_mp4.py
@@ -0,0 +1,25 @@
+import os.path as path
+from lib.perfBaseTest import PerfBaseTest
+
+
+class TestSikuli(PerfBaseTest):
+
+    def setUp(self):
+        super(TestSikuli, self).setUp()
+        self.set_configs(self.config_name.INDEX,
+                         self.extract_platform_dep_settings({'win32': {'7': {'compare-threshold': 0.005},
+                                                                       '10': {'compare-threshold': 0.005}}}))
+
+    def test_firefox_media_ail_click_playback_mp4(self):
+        target_file_name = 'TTF_720p.html'
+        self.test_url = path.join(path.dirname(path.realpath(__file__)), target_file_name)
+
+        self.round_status = self.sikuli.run_test(self.env.test_name,
+                                                 self.env.output_name,
+                                                 test_target=self.test_url,
+                                                 script_dp=self.env.test_script_py_dp,
+                                                 args_list=[self.env.img_sample_dp,
+                                                            self.env.img_output_sample_1_fn,
+                                                            self.env.DEFAULT_VIDEO_RECORDING_WIDTH,
+                                                            self.env.DEFAULT_VIDEO_RECORDING_HEIGHT,
+                                                            self.env.DEFAULT_TIMESTAMP])
